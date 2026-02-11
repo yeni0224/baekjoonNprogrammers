7494061c8e5169c962393896af8ee3f8a0cd69ea
yeni0224 <121008881+yeni0224@users.noreply.github.com>
2026-02-11 17:40:34 +0900

    [level 1] Title: 직사각형 별찍기, Time: 4.21 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ C++/프로그래머스/C++/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    
+    cin >> a >> b;
+    
+    for(int j = 0; j < b; j++)
+    {
+        for(int i = 0; i < a; i++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
